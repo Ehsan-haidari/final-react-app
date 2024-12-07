89bb24fd6195bd345611a11bb07bf4a73b3aae3b
Ehsan <Ehsan.haidari6767@gamil.com>
2024-12-07 22:03:06 +0430

    first commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,43 @@
+import Pepperoni from "../assets/Pepperoni.jfif";
+import Margherita from "../assets/Margherita.jpg";
+import PedroTechSpecial from "../assets/PedroTechSpecial.jfif";
+import Vegan from "../assets/Vegan.jfif";
+import Pineapple from "../assets/Pineapple.jfif";
+import Expensive from "../assets/VaryExpensive.jfif";
+
+export  const MenuList = [
+    {
+        name:" Pepperoni Pizza ",
+        image:  Pepperoni,
+        price: 150
+    },
+    {
+        name:" Margherita Pizza ",
+        image:  Margherita,
+        price: 170
+    },
+    {
+        name:" PedroTech Special Pizza ",
+        image:  PedroTechSpecial,
+        price: 190
+    },
+    {
+        name:" Vegan Pizza ",
+        image:  Vegan,
+        price: 210
+    },
+    {
+        name:" Pineapple Pizza ",
+        image: Pineapple,
+        price: 230
+    },
+    {
+        name:" Vary Expensive Pizza ",
+        image:  Expensive,
+        price: 350
+    },
+    
+      
+    
+];
+
